9b52d48690dc032ee21ead7cbf9a45a778378f63
Emanuil <eiv.razgrad@gmail.com>
2023-02-14 20:34:23 +0200

    Simple learn and test.

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,29 @@ public class Main {
         System.out.println("Hello world! I am Emanuil from Razgrad.");
     }
 }
+
+class HelloJava {
+    public static void main(String[] arguments) {
+        System.out.println("Hello, Java");
+    }
+}
+
+class TestVariables{
+
+    public static void main(String[] arguments){
+        // Declare some variables
+        byte centuries = 20;
+        short years = 2000;
+        int days = 730480;
+        long hours = 17531520;
+        // Print the result on the console
+        System.out.println(centuries + " centuries is " + years +
+                " years, or " + days + " days, or " + hours + " hours.");
+
+        int a = 1;
+        int b = 123;
+
+        System.out.println(a == b);
+    }
+
+}
